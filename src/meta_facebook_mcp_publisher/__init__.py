62877eb96d73fb981142b057a15b403ed256a9ad
jadeldiaz01-png <jadeldiaz01@gmail.com>
2026-08-27 09:04:27 -0400

    Add social trend intelligence: src/meta_facebook_mcp_publisher/__init__.py

--- src/meta_facebook_mcp_publisher/__init__.py
+++ src/meta_facebook_mcp_publisher/__init__.py
@@ -7,6 +7,18 @@ from .models import Platform, PlatformCapability, PlatformPublishRequest, Publis
 from .orchestrator import MultiPlatformPublisher
 from .platform_clients import TikTokClient, XClient, YouTubeClient
 from .training_pipeline import TrainingPlan, build_training_plan, training_layers
+from .trend_intelligence import (
+    CreativeBrief,
+    PlatformTrendSpec,
+    TrendCandidate,
+    TrendDecisionStatus,
+    TrendScore,
+    TrendSignal,
+    build_creative_brief,
+    list_required_trend_layers,
+    rank_creative_briefs,
+    score_trend_candidate,
+)
 
 __all__ = [
     "DataSource",
@@ -17,24 +29,34 @@ __all__ = [
     "AIProviderGateway",
     "AIRequest",
     "AIResponse",
+    "CreativeBrief",
     "MetaFacebookClient",
     "MultiPlatformPublisher",
     "PublishGateDecision",
     "Platform",
     "PlatformCapability",
     "PlatformPublishRequest",
+    "PlatformTrendSpec",
     "PublishRequest",
     "PublishResult",
     "SourceDecision",
     "SourcePolicyResult",
     "TikTokClient",
+    "TrendCandidate",
+    "TrendDecisionStatus",
     "TrainingPlan",
+    "TrendScore",
+    "TrendSignal",
     "XClient",
     "YouTubeClient",
+    "build_creative_brief",
     "build_training_plan",
     "evaluate_data_source",
     "evaluate_platform_publish_gates",
     "evaluate_publish_gates",
+    "list_required_trend_layers",
     "production_training_allowed",
+    "rank_creative_briefs",
+    "score_trend_candidate",
     "training_layers",
 ]
