3afab031415940b02c1a517f027438a5b9672e93
jadeldiaz01-png <jadeldiaz01@gmail.com>
2026-08-27 08:46:17 -0400

    Export ML training governance APIs

--- src/meta_facebook_mcp_publisher/__init__.py
+++ src/meta_facebook_mcp_publisher/__init__.py
@@ -1,10 +1,16 @@
 from .client import MetaFacebookClient
+from .data_governance import DataSource, SourceDecision, SourcePolicyResult, evaluate_data_source
+from .dataset_registry import DatasetRecord, DatasetRegistry, production_training_allowed
 from .gates import PublishGateDecision, evaluate_platform_publish_gates, evaluate_publish_gates
 from .models import Platform, PlatformCapability, PlatformPublishRequest, PublishRequest, PublishResult
 from .orchestrator import MultiPlatformPublisher
 from .platform_clients import TikTokClient, XClient, YouTubeClient
+from .training_pipeline import TrainingPlan, build_training_plan, training_layers
 
 __all__ = [
+    "DataSource",
+    "DatasetRecord",
+    "DatasetRegistry",
     "MetaFacebookClient",
     "MultiPlatformPublisher",
     "PublishGateDecision",
@@ -13,9 +19,16 @@ __all__ = [
     "PlatformPublishRequest",
     "PublishRequest",
     "PublishResult",
+    "SourceDecision",
+    "SourcePolicyResult",
     "TikTokClient",
+    "TrainingPlan",
     "XClient",
     "YouTubeClient",
+    "build_training_plan",
+    "evaluate_data_source",
     "evaluate_platform_publish_gates",
     "evaluate_publish_gates",
+    "production_training_allowed",
+    "training_layers",
 ]
